ca20f7d2d67df2018821a46f5067796d2fb1c629
Ash7gaut <antoningautier@outlook.fr>
2024-11-13 20:51:50 +0100

    front localhost to rankedtracking

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001' ;
+const API_URL = process.env.REACT_APP_API_URL || 'https://rankedtracking-backend.onrender.com' ;
 
 export const api = {
   getPlayers: async () => {
